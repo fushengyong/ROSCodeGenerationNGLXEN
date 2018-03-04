736ddafe7cc100df14b642ea6cd2fabcf6ef13b8
Tóth Csaba <tothcs1105@gmail.com>
2018-03-04 04:08:13 +0100

    Rosbridge client integration tests added: Publisher and Subscriber.

--- Rosbridge.Client.V2_0/AssemblyInfo.cs
+++ Rosbridge.Client.V2_0/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Rosbridge.Client.V2_0.UnitTests")]
+[assembly: InternalsVisibleTo("Rosbridge.Client.V2_0.IntegrationTests")]
